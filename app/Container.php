d351fb2a26027c77aac69ae265c285a75b6e3470
Abdulkadir Poyraz <kadirpoyraz96@gmail.com>
2020-08-10 01:56:20 +0300

    remove unused relation

--- app/Container.php
+++ app/Container.php
@@ -3,14 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Container extends Model
 {
     protected $guarded = [];
-
-    public function tradeflow(): BelongsToMany
-    {
-        return $this->belongsToMany(Tradeflow::class, 'tradeflow_container');
-    }
 }
