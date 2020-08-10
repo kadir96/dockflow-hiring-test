c848e378a947bbf89598505210a641d55f2857d0
Abdulkadir Poyraz <kadirpoyraz96@gmail.com>
2020-08-10 03:25:27 +0300

    report exception if one is thrown in ImportController

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -14,6 +14,7 @@ class ImportController extends Controller
         try {
             $importResult = Importer::forFile($request->file->path())->import();
         } catch (ImportException $e) {
+            report($e);
             abort(500, 'Import failed!');
         }
 
